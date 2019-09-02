a7762b2abfcf9bcee629b22411fe809fa7a4beb2
Citruspress <simon.hemaker@gmail.com>
2019-09-02 22:55:40 +0200

    Use thread_local! to keep state between js calls

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,30 @@
 mod chip8;
 mod webgl;
 
+use std::cell::RefCell;
 use wasm_bindgen::prelude::*;
+
+struct Data {
+    pub renderer: chip8::Renderer,
+    pub screen: chip8::Screen,
+}
 
 #[wasm_bindgen]
 pub fn start() -> Result<(), JsValue> {
-    let renderer = chip8::Renderer::new()?;
-    let mut screen = chip8::Screen::new();
+    thread_local! {
+        static DATA: RefCell<Data> = RefCell::new(Data {
+            renderer: chip8::Renderer::new().expect("failed to initialize renderer"),
+            screen: chip8::Screen::new(),
+        });
+    }
 
-    renderer.render(&screen);
+    DATA.with(|data| {
+        let mut data = data.borrow_mut();
+        data.screen.clear();
+        data.screen.draw_sprite(5, 5, &vec![0xff, 0xff, 0xff]);
+
+        data.renderer.render(&data.screen);
+    });
 
     Ok(())
 }
